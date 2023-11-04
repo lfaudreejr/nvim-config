0b5cd7b4d99fe0f595e175c74e9a8f1a80b23e6a
lfaudreejr <25397017+lfaudreejr@users.noreply.github.com>
2023-11-03 21:48:25 -0400

    and cmp symbols for menu

--- lua/user/lsp/cmp.lua
+++ lua/user/lsp/cmp.lua
@@ -40,6 +40,13 @@ function CMP.setup()
 						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 						symbol_map = { Copilot = "" },
+						menu = {
+							buffer = "[BUF]",
+							nvim_lsp = "[LSP]",
+							nvim_lua = "[LUA]",
+							path = "[PATH]",
+							luasnip = "[SNIP]",
+						},
 
 						-- The function below will be called before any actual modifications from lspkind
 						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
