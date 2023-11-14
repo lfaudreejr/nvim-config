ea6003105d3961ac31d739266e3bbe3cb8967d38
lfaudreejr <25397017+lfaudreejr@users.noreply.github.com>
2023-11-14 16:25:12 -0500

    whichkey stuff and conjure

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -8,10 +8,29 @@ return {
 		"gpanders/nvim-parinfer",
 		ft = { "clojure" },
 		init = function()
-		  vim.g.parinfer_force_balance = true
+			vim.g.parinfer_force_balance = true
 			vim.g.parinfer_comment_chars = ";;"
 		end,
 	},
+	-- {
+	-- 	"Olical/conjure",
+	-- 	ft = { "clojure", "fennel", "python" }, -- etc
+	-- 	-- [Optional] cmp-conjure for cmp
+	-- 	dependencies = {
+	-- 		{
+	-- 			"PaterJason/cmp-conjure",
+	-- 		},
+	-- 	},
+	-- 	config = function()
+	--      vim.g["conjure#mapping#doc_word"] = ""
+	-- 		require("conjure.main").main()
+	-- 		require("conjure.mapping")["on-filetype"]()
+	-- 	end,
+	-- 	init = function()
+	-- 		-- Set configuration options here
+	-- 		vim.g["conjure#debug"] = true
+	-- 	end,
+	-- },
 	{
 		"Olical/conjure",
 		ft = { "clojure", "fennel", "python" }, -- etc
@@ -19,10 +38,22 @@ return {
 		dependencies = {
 			{
 				"PaterJason/cmp-conjure",
+				config = function()
+					local cmp = require("cmp")
+					local config = cmp.get_config()
+					table.insert(config.sources, {
+						name = "buffer",
+						option = {
+							sources = {
+								{ name = "conjure" },
+							},
+						},
+					})
+					cmp.setup(config)
+				end,
 			},
 		},
-		config = function()
-      vim.g["conjure#mapping#doc_word"] = ""
+		config = function(_, opts)
 			require("conjure.main").main()
 			require("conjure.mapping")["on-filetype"]()
 		end,
