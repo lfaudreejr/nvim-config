4f672a249d19c2aa34d638fca9be30877e15155c
Larry Faudree <larry.faudree@LTG-LJDPXLWL3X.attlocal.net>
2024-03-05 09:54:47 -0500

    ui stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,9 +6,7 @@ return {
 	},
 	{
 		"stevearc/dressing.nvim",
-		enabled = false,
-		event = "VeryLazy",
-		config = true,
+		opts = {},
 	},
 	{
 		"christoomey/vim-tmux-navigator",
@@ -80,5 +78,21 @@ return {
 		"folke/persistence.nvim",
 		event = "BufReadPre",
 		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+	},
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			-- "rcarriga/nvim-notify",
+		},
 	},
 }
