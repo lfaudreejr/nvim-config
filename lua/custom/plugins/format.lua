42592490605687c736b145e56deafda8ef66d307
lfaudreejr <25397017+lfaudreejr@users.noreply.github.com>
2024-03-27 23:01:36 -0400

    undo format on save

--- lua/custom/plugins/format.lua
+++ lua/custom/plugins/format.lua
@@ -26,7 +26,7 @@ return {
 			go = { "gofmt" },
 		},
 		-- Set up format-on-save
-		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		-- format_on_save = { timeout_ms = 500, lsp_fallback = true },
 		-- Customize formatters
 		formatters = {
 			shfmt = {
