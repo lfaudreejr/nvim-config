3f601067d9c1efb49ea74f41c2ab5a49be590241
lfaudreejr <25397017+lfaudreejr@users.noreply.github.com>
2024-07-12 17:15:37 -0400

    blah which-key sucks now

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -56,9 +56,41 @@ return {
 	},
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		event = "VeryLazy", -- Sets the loading event to 'VimEnter'
+		-- opts = {
+		-- 	defaults = {
+		-- 		["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
+		-- 		["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+		-- 		["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+		-- 		["<leader>f"] = { name = "[F]iletree", _ = "which_key_ignore" },
+		-- 		["<leader>l"] = { name = "[L]sp", _ = "which_key_ignore" },
+		-- 		["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+		-- 		["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+		-- 		["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+		-- 	}
+		-- },
 		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>1", hidden = true },
+				{ "<leader>2", hidden = true },
+				{ "<leader>3", hidden = true },
+				{ "<leader>4", hidden = true },
+				{ "<leader>5", hidden = true },
+				{ "<leader>a", hidden = true }, -- group
+				{ "<leader>b", group = "[B]uffer" }, -- group
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>f", group = "[F]iletree" },
+				{ "<leader>l", group = "[L]sp" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
+			})
+			wk.setup({
+				preset = "modern",
+			})
+			-- require("which-key").setup()
 
 			-- Document existing key chains
 			-- require("which-key").register({
