63f7faa203b06bb7fcbb0f3b5094812329d9ccb3
lfaudreejr <25397017+lfaudreejr@users.noreply.github.com>
2024-07-12 16:04:56 -0400

    updates, which-key wtf

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -61,16 +61,16 @@ return {
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>f"] = { name = "[F]iletree", _ = "which_key_ignore" },
-				["<leader>l"] = { name = "[L]sp", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			})
+			-- require("which-key").register({
+			-- 	["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
+			-- 	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			-- 	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			-- 	["<leader>f"] = { name = "[F]iletree", _ = "which_key_ignore" },
+			-- 	["<leader>l"] = { name = "[L]sp", _ = "which_key_ignore" },
+			-- 	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			-- 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			-- 	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			-- })
 		end,
 	},
 	{ "digitaltoad/vim-pug", event = "BufReadPost" },
