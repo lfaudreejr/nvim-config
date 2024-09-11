df9a70ad9b1c00b80a4b20d71253a4374cadf125
Larry Faudree <larry.faudree@LTG-LJDPXLWL3X.attlocal.net>
2024-09-11 13:19:20 -0400

    move notify to bottom right

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,12 @@
 return {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically,
 	{
+		"rcarriga/nvim-notify",
+		opts = {
+			top_down = false,
+		}
+	},
+	{
 		"folke/neodev.nvim",
 		opts = { lspconfig = false },
 	},
@@ -117,6 +123,40 @@ return {
 		event = "VeryLazy",
 		opts = {
 			-- add any options here
+			notify = {
+				enabled = true,
+				view = "notify",
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 5,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+					},
+				},
+			},
 		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
